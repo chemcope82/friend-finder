130db09db30ad39d99c12b0ec90fd0d7147c6102
Chemicalz82 <chemcope82@yahoo.com>
2018-08-29 22:41:57 -0500

    created server and routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+
+
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -8,9 +9,8 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "home.html"));
-});
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function(){
     console.log("App is listening on PORT " + PORT);
