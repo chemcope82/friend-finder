4c07b58082ecb5bc609a808ca70d6262e59a9bfa
Chemicalz82 <chemcope82@yahoo.com>
2018-08-30 09:53:55 -0500

    adding script to html files and tweaking server as needed

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,76 @@
 
-var friends = [];
+var friends = [
+    {
+        name: "Tommy",
+        image: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj88Y3S_pTdAhWimeAKHUkIB50QjRx6BAgBEAU&url=https%3A%2F%2Fpixabay.com%2Fen%2Fphotos%2Fchihuahua%2F&psig=AOvVaw0034HW9qoZCRs6ym7uNcXl&ust=1535725708763924",
+        q1: 3,
+        q2: 4,
+        q3: 5,
+        q4: 1,
+        q5: 2,
+        q6: 3,
+        q7: 3,
+        q8: 4,
+        q9: 5,
+        q10: 5
+    },
+    {
+        name: "Felicia",
+        image: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj88Y3S_pTdAhWimeAKHUkIB50QjRx6BAgBEAU&url=https%3A%2F%2Fpixabay.com%2Fen%2Fphotos%2Fchihuahua%2F&psig=AOvVaw0034HW9qoZCRs6ym7uNcXl&ust=1535725708763924",
+        q1: 2,
+        q2: 4,
+        q3: 5,
+        q4: 5,
+        q5: 4,
+        q6: 3,
+        q7: 3,
+        q8: 3,
+        q9: 5,
+        q10: 4
+    },
+    {
+        name: "Rupert",
+        image: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj88Y3S_pTdAhWimeAKHUkIB50QjRx6BAgBEAU&url=https%3A%2F%2Fpixabay.com%2Fen%2Fphotos%2Fchihuahua%2F&psig=AOvVaw0034HW9qoZCRs6ym7uNcXl&ust=1535725708763924",
+        q1: 1,
+        q2: 5,
+        q3: 4,
+        q4: 2,
+        q5: 3,
+        q6: 3,
+        q7: 3,
+        q8: 5,
+        q9: 5,
+        q10: 2
+    },
+    {
+        name: "Amy",
+        image: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj88Y3S_pTdAhWimeAKHUkIB50QjRx6BAgBEAU&url=https%3A%2F%2Fpixabay.com%2Fen%2Fphotos%2Fchihuahua%2F&psig=AOvVaw0034HW9qoZCRs6ym7uNcXl&ust=1535725708763924",
+        q1: 5,
+        q2: 3,
+        q3: 4,
+        q4: 3,
+        q5: 1,
+        q6: 3,
+        q7: 4,
+        q8: 4,
+        q9: 5,
+        q10: 5
+    },
+    {
+        name: "Katherine",
+        image: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj88Y3S_pTdAhWimeAKHUkIB50QjRx6BAgBEAU&url=https%3A%2F%2Fpixabay.com%2Fen%2Fphotos%2Fchihuahua%2F&psig=AOvVaw0034HW9qoZCRs6ym7uNcXl&ust=1535725708763924",
+        q1: 5,
+        q2: 4,
+        q3: 5,
+        q4: 4,
+        q5: 2,
+        q6: 3,
+        q7: 2,
+        q8: 4,
+        q9: 5,
+        q10: 2
+    },
+
+];
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,15 @@
 
 
-var friends = require("../data/friends");
+var friends = require("../data/friends.js");
 
 module.exports = function(app){
 
     app.get("/api/friends", function(req, res){
-        res.json(friends);
+        return res.json(friends);
     });
 
     app.post("/api/friends", function(req, res){
         friends.push(req.body);
-        res.json(true);
+        return res.json(true);
     });
 }
