dd620342175fc9793817f9923a667acc70a1d739
Chemicalz82 <chemcope82@yahoo.com>
2018-08-31 18:07:11 -0500

    Added logic to choose a friend

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,7 +9,29 @@ module.exports = function(app){
     });
 
     app.post("/api/friends", function(req, res){
+
+        //Logic to compare user survey answers to existing friends options
+        var userScores = req.body.scores;
+        var bestMatch = {
+            leastDif: 100,
+            friend: {}
+        }
+
+        for (var i=0; i<friends.length; i++){
+
+            var totalDif = 0; 
+            for(var j=0; j<9; j++) {
+                var scoreDif = Math.abs(userScores[j] - friends[i].scores[j]);
+                totalDif += scoreDif;
+            }
+            
+            if(totalDif < bestMatch.leastDif) {
+                bestMatch.friend = friends[i];
+                bestMatch.leastDif = totalDif;
+            }
+        }
+
         friends.push(req.body);
-        return res.json(true);
+        res.json(bestMatch);
     });
 }
